c8d5b2ddc6497111c49acdd4ca17483d2fba023b
Matthias Kurz <m.kurz@irregular.at>
2022-03-10 23:36:38 +0100

    Direct the body parser like in the docs
    https://www.playframework.com/documentation/2.8.x/ScalaBodyParsers#Directing-the-body-elsewhere

--- playframework-10937/app/controllers/HomeController.scala
+++ playframework-10937/app/controllers/HomeController.scala
@@ -1,13 +1,26 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
+import play.api.libs.streams._
+import play.api.libs.ws._
+import scala.concurrent.ExecutionContext
+import akka.util.ByteString
 
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(ws: WSClient, val controllerComponents: ControllerComponents)(
+  implicit ec: ExecutionContext
+) extends BaseController {
+  def forward(request: WSRequest): BodyParser[WSResponse] = BodyParser { req =>
+    Accumulator.source[ByteString].mapFuture { source =>
+      request
+        .withBody(source)
+        .execute("POST")
+        .map(Right.apply)
+    }
+  }
 
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("hello world")
+  def index() = Action(forward(ws.url("http://localhost:9001"))) { req =>
+    Ok("Uploaded")
   }
 }
