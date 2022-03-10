aaa8322dae7f644c85afc2b388939b895a24eea2
Matthias Kurz <m.kurz@irregular.at>
2022-03-10 23:41:57 +0100

    Debug body with printlns ;)

--- playframework-10937-body-redirect-to-server/app/controllers/HomeController.scala
+++ playframework-10937-body-redirect-to-server/app/controllers/HomeController.scala
@@ -1,13 +1,24 @@
 package controllers
 
+import akka.util.ByteString
+
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+    println("Headers:")
+    println("--------")
+    request.headers.headers.foreach(header => println(s"${header._1}: ${header._2}"))
+    println()
+    println("Body size: " + request.body.asRaw.size)
+    println()
+    println("Body:")
+    println("-----")
+    println(request.body.asRaw.flatMap(_.asBytes()).getOrElse(ByteString.emptyByteString).utf8String)
+    println()
+    Ok("hello world!")
   }
 }
