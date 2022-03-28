d785cee186545181897fd3d7b2a8870e860be2bd
Matthias Kurz <m.kurz@irregular.at>
2022-03-28 22:35:11 +0200

    flags for playframework#11209 (run "test")

--- playframework-11209/build.sbt
+++ playframework-11209/build.sbt
@@ -10,3 +10,16 @@ scalaVersion := "2.13.8"
 libraryDependencies += guice
 libraryDependencies += "com.google.inject" % "guice" % "5.1.0"
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+
+Test / javaOptions ++= Seq(
+  "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9998",
+  "-Xms512M",
+  "-Xmx1536M",
+  "-Xss1M",
+)
+
+Test / fork := true // This is the default anyway
+Test / javaOptions ++= Seq(
+  "--add-exports=java.base/sun.security.x509=ALL-UNNAMED",
+  //"--add-opens=java.base/sun.security.ssl=ALL-UNNAMED"
+)
--- playframework-11209/test/controllers/HomeControllerSpec.scala
+++ playframework-11209/test/controllers/HomeControllerSpec.scala
@@ -1,7 +1,9 @@
 package controllers
 
+import org.scalatest.TestData
 import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
+import play.api.Application
 import play.api.test._
 import play.api.test.Helpers._
 
@@ -11,31 +13,14 @@ import play.api.test.Helpers._
  *
  * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
  */
-class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+class HomeControllerSpec extends PlaySpec with GuiceOneServerPerTest with Injecting {
 
   "HomeController GET" should {
 
     "render the index page from a new instance of controller" in {
+      System.out.println(port) // "debugging" ;)
       val controller = new HomeController(stubControllerComponents())
       val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the application" in {
-      val controller = inject[HomeController]
-      val home = controller.index().apply(FakeRequest(GET, "/"))
-
-      status(home) mustBe OK
-      contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
-    }
-
-    "render the index page from the router" in {
-      val request = FakeRequest(GET, "/")
-      val home = route(app, request).get
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
