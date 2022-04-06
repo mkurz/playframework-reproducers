d8829f9b3dade483910615290cab7e081d533725
Matthias Kurz <m.kurz@irregular.at>
2022-04-06 17:03:12 +0200

    Try to reproduce playframework#11220

--- playframework-11220/app/controllers/HomeController.java
+++ playframework-11220/app/controllers/HomeController.java
@@ -1,10 +1,25 @@
 package controllers;
 
+import play.data.FormFactory;
+import play.i18n.Lang;
+import play.libs.typedmap.TypedMap;
 import play.mvc.*;
+
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeController extends Controller {
 
+    private final FormFactory formFactory;
+
+    @Inject
+    public HomeController(final FormFactory formFactory) {
+        this.formFactory = formFactory;
+    }
+
     public Result index() {
+        this.formFactory.form().bind(Lang.defaultLang(), TypedMap.create(), new HashMap<>());
         return ok("hello world");
     }
 
