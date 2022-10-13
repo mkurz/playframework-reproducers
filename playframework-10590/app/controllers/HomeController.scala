1593a10ae4db12ebd77c8bf01e95aa810ce7cb9b
Matthias Kurz <m.kurz@irregular.at>
2022-10-13 12:34:41 +0200

    Reproducer

--- playframework-10590/app/controllers/HomeController.scala
+++ playframework-10590/app/controllers/HomeController.scala
@@ -1,13 +1,17 @@
 package controllers
 
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok("hello world")
+    val checkboxCount = request.getQueryString("checkboxCount").map(_.toInt).getOrElse(10000)
+    Ok(views.html.index(checkboxCount))
+  }
+
+  def testForPost() = Action(parse.multipartFormData) { implicit request: Request[MultipartFormData[play.api.libs.Files.TemporaryFile]] =>
+    Redirect(controllers.routes.HomeController.index())
   }
 }
