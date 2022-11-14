a7c1a125bdb04856f948bc66d3aa0e7511d82b98
Matthias Kurz <m.kurz@irregular.at>
2022-11-14 16:07:32 +0100

    More tests

--- playframework-11496/test/controllers/HomeControllerTest.java
+++ playframework-11496/test/controllers/HomeControllerTest.java
@@ -1,9 +1,11 @@
 package controllers;
 
+import java.io.IOException;
 import java.util.*;
 import org.junit.Test;
 import play.Application;
 import play.inject.guice.GuiceApplicationBuilder;
+import play.libs.Files;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
@@ -21,7 +23,7 @@ public class HomeControllerTest extends WithApplication {
     }
 
     @Test
-    public void testIndex() {
+    public void testEmpty() {
         final Map<String, String[]> postParams = new HashMap<>();
         postParams.put("value", new String[]{"value"});
         Http.RequestBuilder request = new Http.RequestBuilder()
@@ -30,7 +32,49 @@ public class HomeControllerTest extends WithApplication {
                 .uri("/test");
 
         Result result = route(app, request);
+        result.body().consumeData(mat).thenAccept(bs -> System.out.println(bs.utf8String())); // yeah... debugging...
         assertEquals(OK, result.status());
     }
 
+    @Test
+    public void testRealJavaTempFile() throws IOException {
+        Files.TemporaryFile tempFile = Files.singletonTemporaryFileCreator().create("temp", "txt");
+        java.nio.file.Files.write(tempFile.path(), "Twas brillig and the slithy Toves...".getBytes());
+        final List<Http.MultipartFormData.FilePart> files = List.of(
+                new Http.MultipartFormData.FilePart<>(
+                        "document", "jabberwocky.txt", "text/plain", tempFile));
+
+        final Map<String, String[]> postParams = new HashMap<>();
+        postParams.put("value", new String[]{"value"});
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(POST)
+                .bodyMultipart(postParams, files)
+                .uri("/test");
+
+        Result result = route(app, request);
+        result.body().consumeData(mat).thenAccept(bs -> System.out.println(bs.utf8String()));
+        System.out.println();
+        assertEquals(OK, result.status());
+    }
+
+    @Test
+    public void testRealScalaTempFile() throws IOException {
+        play.api.libs.Files.TemporaryFile tempFile = play.api.libs.Files.SingletonTemporaryFileCreator$.MODULE$.create("temp", "txt");
+        java.nio.file.Files.write(tempFile.path(), "Twas brillig and the slithy Toves...".getBytes());
+        final List<Http.MultipartFormData.FilePart> files = List.of(
+                new Http.MultipartFormData.FilePart<>(
+                        "document", "jabberwocky.txt", "text/plain", tempFile));
+
+        final Map<String, String[]> postParams = new HashMap<>();
+        postParams.put("value", new String[]{"value"});
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(POST)
+                .bodyMultipart(postParams, files)
+                .uri("/test");
+
+        Result result = route(app, request);
+        result.body().consumeData(mat).thenAccept(bs -> System.out.println(bs.utf8String()));
+        System.out.println();
+        assertEquals(OK, result.status());
+    }
 }
